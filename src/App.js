a83075699d105a217cc03b8f93d847d85acabd8b
GDharan07 <dharansparrow77@gmail.com>
2024-08-03 10:01:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Home from './Component/Home';
+import About from './Component/About'
+import Order from './Component/Order';
+import Cart from './Component/Cart';
+import Login from './Component/Login';
+import {MyProvider} from './MyContext/MyProvider'
+function App() {
+  return (
+    <div className="App">
+    <MyProvider>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/About' element={<About/>}></Route>
+          <Route path='/Order' element={<Order/>}></Route>
+          <Route path='/Cart' element={<Cart/>}></Route>
+          <Route path='/Login' element={<Login/>}></Route>
+        </Routes>
+     </MyProvider>
+    
+    
+    </div>
+  );
+}
+
+export default App;
+
+
